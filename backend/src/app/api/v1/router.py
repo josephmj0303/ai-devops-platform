0d0fc20794cfc0fed641f765f7c4022490999c89
josephmj0303 <joseph.mj@aol.com>
2026-07-08 06:14:27 +0000

    update router.py to fix auth path issue

--- backend/src/app/api/v1/router.py
+++ backend/src/app/api/v1/router.py
@@ -3,10 +3,5 @@ from fastapi import APIRouter
 from app.api.v1.routes import auth, health
 
 api_router = APIRouter()
-
 api_router.include_router(health.router)
-
-api_router.include_router(
-    auth.router,
-    prefix="/auth",
-)
+api_router.include_router(auth.router)
