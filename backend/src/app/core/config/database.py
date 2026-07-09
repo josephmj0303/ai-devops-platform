f6ba06c9dc6aa73fffd4871c9809a5c21f80cebf
josephmj0303 <joseph.mj@aol.com>
2026-07-09 03:44:26 +0000

    add database.py

---
+++ backend/src/app/core/config/database.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class DatabaseSettings(BaseSettings):
+
+    DATABASE_URL: str = (
+        "postgresql://postgres:postgres@localhost:5432/ai_devops"
+    )
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore",
+    )
