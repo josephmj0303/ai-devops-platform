4fd51a75bdc204e7eed08b3514715bb3d83a0925
josephmj0303 <joseph.mj@aol.com>
2026-07-20 08:13:38 +0000

    edit registry.py data in db

--- backend/src/app/db/registry.py
+++ backend/src/app/db/registry.py
@@ -1,9 +1,5 @@
-from app.models.user import User
-from app.models.role import Role
-from app.models.token import RefreshToken
+"""
+Import all SQLAlchemy models here.
 
-__all__ = [
-    "User",
-    "Role",
-    "RefreshToken",
-]
+Alembic uses these imports to discover metadata.
+"""
