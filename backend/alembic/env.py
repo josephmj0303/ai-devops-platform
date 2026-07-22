157def0b293707a917258a8d218ac5f5293395f6
josephmj0303 <joseph.mj@aol.com>
2026-07-22 08:30:30 +0000

    edit env.py in alembic

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,3 +1,12 @@
+from pathlib import Path
+import sys
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+SRC_DIR = BASE_DIR / "src"
+
+if str(SRC_DIR) not in sys.path:
+    sys.path.insert(0, str(SRC_DIR))
+
 from logging.config import fileConfig
 
 from alembic import context
