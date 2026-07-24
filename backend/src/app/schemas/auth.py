f6063e44347e0f7d9e0f1d1f524f048fc681b084
josephmj0303 <joseph.mj@aol.com>
2026-07-24 05:33:03 +0000

    update auth.py in schemas

---
+++ backend/src/app/schemas/auth.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, EmailStr
+
+
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
+
+class TokenData(BaseModel):
+    id: int
+    email: EmailStr
+    full_name: str
+    is_active: bool
