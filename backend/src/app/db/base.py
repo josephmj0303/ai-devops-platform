df6b266ad120a419876d1a205c7c2069251a5467
josephmj0303 <joseph.mj@aol.com>
2026-07-28 10:48:31 +0000

    add user and project model to base import

--- backend/src/app/db/base.py
+++ backend/src/app/db/base.py
@@ -1,5 +1,7 @@
 from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase
+from app.models.user import User
+from app.models.project import Project
 
 NAMING_CONVENTION = {
     "ix": "ix_%(column_0_label)s",
