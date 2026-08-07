53c99806fa4d9ee9fd7e892ccef3a44d4084b1e0
josephmj0303 <joseph.mj@aol.com>
2026-08-07 07:39:44 +0000

    create authentication api

---
+++ frontend/src/api/auth.ts
@@ -0,0 +1,14 @@
+import api from "./axios";
+import { LoginRequest, LoginResponse } from "../types/auth";
+
+export const login = async (
+  credentials: LoginRequest
+): Promise<LoginResponse> => {
+
+  const response = await api.post<LoginResponse>(
+    "/auth/login",
+    credentials
+  );
+
+  return response.data;
+};
