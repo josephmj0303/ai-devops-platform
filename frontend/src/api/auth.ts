e6b8188037cce8600c484bf15b25daf0941c9432
josephmj0303 <joseph.mj@aol.com>
2026-08-08 11:40:48 +0000

    update auth.ts in api

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -4,10 +4,19 @@ import type { LoginRequest, LoginResponse } from "../types/auth";
 export const login = async (
   credentials: LoginRequest
 ): Promise<LoginResponse> => {
+  const formData = new URLSearchParams();
+
+  formData.append("username", credentials.email);
+  formData.append("password", credentials.password);
 
   const response = await api.post<LoginResponse>(
     "/auth/login",
-    credentials
+    formData,
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
   );
 
   return response.data;
