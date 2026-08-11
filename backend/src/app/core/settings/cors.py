11f99f4309be226fe27a0fbb46a3347da18819cf
josephmj0303 <joseph.mj@aol.com>
2026-08-11 08:05:45 +0000

    add docker frontend origin

--- backend/src/app/core/settings/cors.py
+++ backend/src/app/core/settings/cors.py
@@ -6,6 +6,8 @@ class CORSSettings(BaseSettings):
     ALLOWED_ORIGINS: list[str] = [
         "http://localhost:5173",
         "http://192.168.56.20:5173",
+        "http://localhost:8080",
+        "http://192.168.56.20:8080",
     ]
 
     model_config = SettingsConfigDict(
