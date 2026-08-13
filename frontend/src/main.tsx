d8f37de5a3ab9c8828b19c48ead4914f2000f54f
josephmj0303 <joseph.mj@aol.com>
2026-08-13 17:49:08 +0000

    fix frontend UI

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import "./index.css";
-
 import App from "./App";
-
+import "./index.css";
+import "./App.css";
 import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(
