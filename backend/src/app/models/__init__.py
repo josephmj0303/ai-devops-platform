333d031755762618a899d3a18c0996111da42331
josephmj0303 <joseph.mj@aol.com>
2026-08-18 08:29:02 +0000

    update models

--- backend/src/app/models/__init__.py
+++ backend/src/app/models/__init__.py
@@ -1,9 +1,13 @@
-from .role import UserRole
-from .user import User
+from app.models.ai_analysis import AIAnalysis
+from app.models.devops_action import DevOpsAction
 from app.models.project import Project
+from app.models.role import UserRole
+from app.models.user import User
 
 __all__ = [
     "User",
     "UserRole",
     "Project",
+    "AIAnalysis",
+    "DevOpsAction",
 ]
